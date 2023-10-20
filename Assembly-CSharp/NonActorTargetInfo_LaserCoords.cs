735d1f387ceba76731fbb7dd58f9976ddf7dfb98
mts508 <mathyasrodriguez@gmail.com>
2023-10-20 08:30:25 -0300

    Add NonActorTargetInfo_LaserCoords

---
+++ Assembly-CSharp/NonActorTargetInfo_LaserCoords.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+internal class NonActorTargetInfo_LaserCoords : NonActorTargetInfo
+{
+    public VectorUtils.LaserCoords m_laserCoords;
+
+    public NonActorTargetInfo_LaserCoords(VectorUtils.LaserCoords laserCoords)
+    {
+        this.m_laserCoords = laserCoords;
+    }
+}
